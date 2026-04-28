1bb546caae6f6330171b9ff5fc6703f2203a9732
Candid Dauth <cdauth@cdauth.eu>
2026-04-28 10:58:01 +0200

    Test SIGTERM handling

--- run.sh
+++ run.sh
@@ -38,7 +38,7 @@ chown _renderd:_renderd /data/tiles
 
 # Run while handling docker stop's SIGTERM
 stop_handler() {
-    kill -TERM "$child"
+    # kill -TERM "$child"
 	service apache2 stop
 	service renderd stop
 	exit 0
@@ -47,5 +47,4 @@ trap stop_handler SIGTERM
 
 service renderd start
 
-tail -f /dev/null &
-child=$!
+tail -f /dev/null
